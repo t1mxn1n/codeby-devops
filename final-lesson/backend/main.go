fc4eac94d4e86c06cb8ad5f1881094ca94c99695
nikita <nikita@example.com>
2025-10-25 11:34:13 +0700

    hot fix

--- final-lesson/backend/main.go
+++ final-lesson/backend/main.go
@@ -46,7 +46,7 @@ func main() {
 	http.HandleFunc("/api/load", simulateLoad)
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintln(w, "ok [bug]")
+		fmt.Fprintln(w, "ok health")
 	})
 
 	port := getEnv("PORT", "8080")
